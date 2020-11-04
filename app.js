45b71ad2eebff7bc1895329b20b9bf195bc6d8eb
tekerene <tekerene9@gmail.com>
2020-11-04 18:59:26 +0100

    linked db update

--- app.js
+++ app.js
@@ -38,10 +38,10 @@ app.use('/api/public', indexRouter);
 app.use('/users', usersRouter);
 
 // const port = 3000;
-const port = process.env.PORT || 3000;
-const server = app.listen(port, () => {
-  console.log('Connected to port ' + port)
-})
+// const port = process.env.PORT || 3000;
+// const server = app.listen(port, () => {
+//   console.log('Connected to port ' + port)
+// })
 
-//module.exports = app;
+module.exports = app;
 
